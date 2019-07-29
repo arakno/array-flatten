d519872f52168a5b705e7377b404f24f8642d5a5
paulo basto <paulobasto@gmail.com>
2019-07-29 23:59:14 +0100

    ini

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,4 +1,33 @@
-import '../sass/styles.scss';
+// import '../sass/styles.scss';
 
-console.log('Webpack Boilerplate');
+// function flatten(arr, depth) {
+//     if(!depth) {
+//         return arr.slice();
+//     }
+//     const flatArr = [];
+//     arr.forEach(value => {
+//         if(Array.isArray(value)) {
+//             Array.prototype.concat.apply(flatArr, value);
+//             flatArr.push(...flatten(value, depth - 1));
+//         } else {
+//             flatArr.push(value);
+//         }
+//     });
+// }
 
+function flatten(arr) {
+debugger
+    const flatArr;
+    arr.forEach(value => {
+        if(Array.isArray(value)) {
+            Array.prototype.concat.apply(flatArr, value);
+        } else {
+            flatArr.push(value);
+        }
+    });
+
+    return flatArr;
+}
+
+let initial = [[0, 1], [2, 3], [4, [5, 6, 7]]];
+flatten(initial);
