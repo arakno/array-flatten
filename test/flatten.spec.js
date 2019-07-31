8521f52583044f02d86aa87e05a543d0c4fdd78f
paulo basto <paulobasto@gmail.com>
2019-07-31 23:45:16 +0100

    cleanup

---
+++ test/flatten.spec.js
@@ -0,0 +1,10 @@
+import test from 'tape';
+import flatten from '../src/javascript/index';
+
+const initial = [0, 1, [2, 3], [4, [5, 6, 7]]];
+const expected = [0, 1, 2, 3, 4, 5, 6, 7];
+
+test('Flattens array of nested integers', function(assert) {
+  assert.equal(flatten(initial), expected, 'arrays should be equal');
+  assert.end();
+});
